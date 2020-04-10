337871253d0eb6eee559869e2464e1317f851792
franck <carnet.franck.paul@gmail.com>
2020-04-10 13:34:39 +0200

    Add support/details URL

--- _define.php
+++ _define.php
@@ -15,10 +15,14 @@ $this->registerModule(
     "AddToAny",                                          // Name
     "Add AddToAny sharing tool to your posts and pages", // Description
     "Franck Paul",                                       // Author
-    '0.4',                                             // Version
+    '0.4',                                               // Version
     [
-        'requires'    => [['core', '2.13']], // Dependencies
-        'permissions' => 'admin',            // Permissions
-        'type'        => 'plugin'           // Type
+        'requires'    => [['core', '2.13']],                        // Dependencies
+        'permissions' => 'admin',                                   // Permissions
+        'type'        => 'plugin',                                  // Type
+        'details'     => 'https://open-time.net/?q=addToAny',       // Details URL
+        'support'     => 'https://github.com/franck-paul/addToAny', // Support URL
+        'settings'    => [                                          // Settings
+        ]
     ]
 );
