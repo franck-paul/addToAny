741340342cae36707bca0557c463841d6ec2a22d
franck <carnet.franck.paul@gmail.com>
2021-10-23 12:43:09 +0200

    Add alternative repository

--- _define.php
+++ _define.php
@@ -10,19 +10,24 @@
  * @copyright Franck Paul carnet.franck.paul@gmail.com
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
+if (!defined('DC_RC_PATH')) {
+    return;
+}
 
 $this->registerModule(
-    "AddToAny",                                          // Name
-    "Add AddToAny sharing tool to your posts and pages", // Description
-    "Franck Paul",                                       // Author
+    'AddToAny',                                          // Name
+    'Add AddToAny sharing tool to your posts and pages', // Description
+    'Franck Paul',                                       // Author
     '0.4',                                               // Version
     [
         'requires'    => [['core', '2.13']],                        // Dependencies
         'permissions' => 'admin',                                   // Permissions
         'type'        => 'plugin',                                  // Type
-        'details'     => 'https://open-time.net/?q=addToAny',       // Details URL
-        'support'     => 'https://github.com/franck-paul/addToAny', // Support URL
-        'settings'    => [                                          // Settings
-        ]
+        'settings'    => [// Settings
+        ],
+
+        'details'    => 'https://open-time.net/?q=addToAny',       // Details URL
+        'support'    => 'https://github.com/franck-paul/addToAny', // Support URL
+        'repository' => 'https://raw.githubusercontent.com/franck-paul/addToAny/main/dcstore.xml'
     ]
 );
