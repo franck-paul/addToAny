db6f33f8488957230e30957a65b0e3c808b059a9
franck <carnet.franck.paul@gmail.com>
2023-08-23 11:08:17 +0200

    New release 3.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'AddToAny',
     'Add AddToAny sharing tool to your posts and pages',
     'Franck Paul',
-    '2.2',
+    '3.0',
     [
         'requires'    => [['core', '2.27'], ['php', '8.1']],
         'permissions' => dcCore::app()->auth->makePermissions([
