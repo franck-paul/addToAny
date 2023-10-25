94faab475a7e882d30dc5d19271e0640eeeaa759
franck <carnet.franck.paul@gmail.com>
2023-10-25 12:12:59 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'AddToAny',
     'Add AddToAny sharing tool to your posts and pages',
     'Franck Paul',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
