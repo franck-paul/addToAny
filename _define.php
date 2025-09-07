78b5a4e6cb2419edc5843736bba0be185bd5a11d
franck <carnet.franck.paul@gmail.com>
2025-09-07 18:39:46 +0200

    DC 2.36 compliance

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     '4.2',
     [
         'date'        => '2003-08-13T13:42:00+0100',
-        'requires'    => [['core', '2.28']],
+        'requires'    => [['core', '2.36']],
         'permissions' => 'My',
         'type'        => 'plugin',
         'settings'    => [
@@ -28,5 +28,6 @@ $this->registerModule(
         'details'    => 'https://open-time.net/?q=addToAny',
         'support'    => 'https://github.com/franck-paul/addToAny',
         'repository' => 'https://raw.githubusercontent.com/franck-paul/addToAny/main/dcstore.xml',
+        'license'    => 'gpl2',
     ]
 );
